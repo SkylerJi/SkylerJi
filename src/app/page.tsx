9e5fe9023f9b58f5c693002205027baa7e4ac93b
SkylerJi <skylerji@gmail.com>
2023-11-19 15:51:54 -0500

    ps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      I'm Skyler Ji, yo.
+      <div>
+        I'm Skyler Ji, yo.
+
+      </div>
+     
       <Image src = "/assets/images/Eyebrows.png"
                   width={200}
                   height={200}
