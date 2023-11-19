fa651b0781827915847e4dab76ef06fa2b2de74f
SkylerJi <skylerji@gmail.com>
2023-11-19 16:05:48 -0500

    loser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,46 @@ export default function Home() {
   return (
     <div>
       <div>
-        I'm Skyler Ji, yo.
+        My name is Skyler Ji, yo.
 
       </div>
-     
-      <Image src = "/assets/images/Eyebrows.png"
+     <div>
+     <Image src = "/assets/images/Eyebrows.png"
                   width={200}
                   height={200}
                   alt="Yo"/>
+
+     </div>
+    
+
+      <div>
+        My husband is Your Mom's Wide, yo.
+      </div>
+
+      <div>
+        ...
+      </div>
+
+      <div className = "flex flex-row">
+      And just so you know my
+      brother-in-real is a 
+      <div className = " mx-1" style={{textDecorationLine: 'line-through', textDecorationStyle: 'solid'}}>
+       DEA agent
+        </div>
+        loser 
+        </div>
+
+    <div>
+     <Image src = "/assets/images/Loser.png"
+                  width={200}
+                  height={200}
+                  alt="Yo"/>
+
+     </div>
+
+    
+
+
     </div>
   )
 }
