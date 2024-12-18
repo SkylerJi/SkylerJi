3395723b59749d1de16897b7b3c43c53c871fa04
SkylerJi <skylerji@gmail.com>
2024-12-18 20:29:44 +0800

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,52 +11,52 @@ export default function Home() {
             Things I have thousands of hours doing and usually enjoy.
           </div>
           <div className = "grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-16 md:gap-y-32 text-3xl font-thin pb-32 md:pb-64">
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Accounting
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Startups
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Hockey
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Coding
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Tennis
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Table Tennis
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Pickleball
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Basketball
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Chess
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Bonk.io
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Souls-Like 
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Elder Scrolls
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Anime
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Pokemon (Go/Cards)
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               BTD
             </span>
-            <span className="flex items-center justify-center">
+            <span className="flex items-center justify-center text-center">
               Long Distance Relationships
             </span>
           </div>
