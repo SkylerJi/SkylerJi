07f7ff0affcd6832f54726c212f0f5c1a16c440e
SkylerJi <skylerji@gmail.com>
2024-12-23 15:09:30 +0800

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,19 @@ export default function Home() {
             </div>
         </div>
         <div>
-          <div className = "flex items-center justify-center text-3xl font-thin pb-8 text-center md:pb-16">
-            Things I have thousands of hours doing and usually enjoy.
+          <div className = "flex items-center justify-center text-3xl font-thin pb-16 text-center md:pb-32">
+            Everything I've ever done loosely ranked in terms of current enjoyment. I'm probably forgetting a lot. 
           </div>
           <div className = "grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-16 md:gap-y-32 text-3xl font-thin pb-32 md:pb-64">
-            <span className="flex items-center justify-center text-center">
-              Accounting
-            </span>
+
             <span className="flex items-center justify-center text-center">
               Startups
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Basketball
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Tennis
             </span>
             <span className="flex items-center justify-center text-center">
               Hockey
@@ -24,7 +28,13 @@ export default function Home() {
               Coding
             </span>
             <span className="flex items-center justify-center text-center">
-              Tennis
+              Chess
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Podcasts
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Reading
             </span>
             <span className="flex items-center justify-center text-center">
               Table Tennis
@@ -33,31 +43,79 @@ export default function Home() {
               Pickleball
             </span>
             <span className="flex items-center justify-center text-center">
-              Basketball
+              Consume Media
             </span>
             <span className="flex items-center justify-center text-center">
-              Chess
+              Accounting
             </span>
             <span className="flex items-center justify-center text-center">
               Bonk.io
             </span>
             <span className="flex items-center justify-center text-center">
-              Souls-Like 
+              Sekiro
             </span>
             <span className="flex items-center justify-center text-center">
-              Elder Scrolls
+              Elden Ring
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Dark Souls
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Elder Scrolls Online
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Skyrim
             </span>
             <span className="flex items-center justify-center text-center">
               Anime
             </span>
             <span className="flex items-center justify-center text-center">
-              Pokemon (Go/Cards)
+              Pokemon Cards
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Pokemon Go
             </span>
             <span className="flex items-center justify-center text-center">
               BTD
             </span>
             <span className="flex items-center justify-center text-center">
-              Long Distance Relationships
+              Rock Climbing
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Frisbee
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Skydiving
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Content Creation
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Mope.io
+            </span>
+            <span className="flex items-center justify-center text-center">
+              MooMoo.io
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Karate
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Traveling
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Baseball
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Swimming
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Other Video Games
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Violin
+            </span>
+            <span className="flex items-center justify-center text-center">
+              Piano
             </span>
           </div>
         </div>
